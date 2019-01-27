53dde83add448a89bb40cc1262fe7cac66257684
Yvo Weidmann <33548040+yweidmann@users.noreply.github.com>
2019-01-27 15:53:43 +0100

    Removing not needed imports

--- dataflow/insertDatabaseMassbalance.py
+++ dataflow/insertDatabaseMassbalance.py
@@ -6,9 +6,7 @@ Created on 18.05.2018
 Main script to import all VAW mass-balance data files into the GLAMOS database.
 '''
 
-from dataflow.DataWriters.FileWriters.Database.LengthChangeWriter import CopyLengthChangeData
 from dataflow.DataWriters.DatabaseWriters.MassBalanceWriter import MassBalanceWriter
-from dataflow.DataReaders.VawFileReaders.LengthChangeReader import LengthChangeReader
 from dataflow.DataReaders.VawFileReaders.MassBalanceReader import MassBalanceReader
 from dataflow.DataReaders.DatabaseReaders.GlacierReader import GlacierReader
 from dataflow.DataObjects.Exceptions.GlacierNotFoundError import GlacierNotFoundError
