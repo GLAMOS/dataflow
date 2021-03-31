6d120326ebdd492bd50fc4d40ea9a60b3aae0db2
Elias Hodel <hodel@vaw.baug.ethz.ch>
2021-03-31 16:44:53 +0200

    new Object, Reader and Writer for Mass Balance Point data

--- dataflow/DataReaders/VawFileReaders/MassBalancePointReader.py
+++ dataflow/DataReaders/VawFileReaders/MassBalancePointReader.py
@@ -8,6 +8,7 @@ import re
 
 from dataflow.DataReaders.VawFileReaders.VawFileReader import VawFileReader
 from dataflow.DataObjects.Exceptions.GlacierNotFoundError import GlacierNotFoundError
+from dataflow.DataObjects.Exceptions.MassBalanceError import ObservationTypeNotDefinedError
 from dataflow.DataObjects.MassBalancePoint import MassBalancePoint
 from dataflow.DataReaders.Exceptions.InvalidDataFileError import InvalidDataFileError
 from dataflow.DataObjects.Enumerations.MassBalancePointEnumerations import *
@@ -35,6 +36,7 @@ class MassBalancePointReader(VawFileReader):
     __FILE_COLUMN_TIME_FROM = 2
     __FILE_COLUMN_DATE_TO = 3
     __FILE_COLUMN_TIME_TO = 4
+    __FILE_COLUMN_PERIOD = 5
     __FILE_COLUMN_DATE_QUALITY = 6
     __FILE_COLUMN_LATITUDE = 7
     __FILE_COLUMN_LONGITUDE = 8
@@ -50,6 +52,9 @@ class MassBalancePointReader(VawFileReader):
     __FILE_COLUMN_READING_ERROR = 18
     __FILE_COLUMN_DENSITY_ERROR = 19
     __FILE_COLUMN_SOURCE = 20
+
+    _massBalancePointType = ObservationTypeEnum.NotDefinedUnknown
+    _massBalancePointCounter = 0
 
     def __init__(self, config, fullFileName, glaciers):
         '''
@@ -71,19 +76,23 @@ class MassBalancePointReader(VawFileReader):
         self._numberHeaderLines = self.__NUMBER_HEADER_LINES
 
         # Check if the given file is a correct point mass balance file.
-        searchResult = re.search(config.get("MassbalancePoint", "annualPatternFilename"), self._fullFileName)
+        isMassbalancePointFile = False
+        searchResult = re.search(config.get("MassbalancePoint", "annualPatternFilename"), fullFileName)
         if searchResult != None:
             self._ObservationType = ObservationTypeEnum.Annual
+            isMassbalancePointFile = True
 
-        searchResult = re.search(config.get("MassbalancePoint", "wintersnowPatternFilename"), self._fullFileName)
+        searchResult = re.search(config.get("MassbalancePoint", "wintersnowPatternFilename"), fullFileName)
         if searchResult != None:
             self._ObservationType = ObservationTypeEnum.Wintersnow
+            isMassbalancePointFile = True
 
-        searchResult = re.search(config.get("MassbalancePoint", "intermediatePatternFilename"), self._fullFileName)
+        searchResult = re.search(config.get("MassbalancePoint", "intermediatePatternFilename"), fullFileName)
         if searchResult != None:
             self._ObservationType = ObservationTypeEnum.Intermediate
+            isMassbalancePointFile = True
 
-        if searchResult == None:
+        if searchResult == None and isMassbalancePointFile == False:
             message = "The file {0} is not a point mass balance data file.".format(fullFileName)
             raise InvalidDataFileError(message)
         # TODO: Additional test for file check to be included. If possible, implementation in a generic way in super-class VawFileReader.
@@ -98,12 +107,144 @@ class MassBalancePointReader(VawFileReader):
         pass
 
     def parse(self):
-        # TODO: write parse method
+        with open(self._fullFileName, "r") as mbp:
+
+            lineCounter = 0
+            self._numberDataLines = 0
+
+            dataLines = []
+            if self._ObservationType == ObservationTypeEnum.Annual:
+                observationType = 1
+            elif self._ObservationType == ObservationTypeEnum.Wintersnow:
+                observationType = 2
+            elif self._ObservationType == ObservationTypeEnum.Intermediate:
+                observationType = 3
+            else:
+                message = "Not defined point mass balance type of file {0}".format(self._fullFileName)
+                raise ObservationTypeNotDefinedError(message)
+
+            for line in mbp:
+
+                lineCounter += 1
+
+                try:
+
+                    if lineCounter > self.__NUMBER_HEADER_LINES:
+                        data = self._getData(line)
+
+                        massBalancePoint = MassBalancePoint(
+                            pk=None,
+                            name=data[self.__FILE_COLUMN_NAME],
+                            observationType=observationType,
+                            dateFrom=data[self.__FILE_COLUMN_DATE_FROM], timeFrom=data[self.__FILE_COLUMN_TIME_FROM],
+                            dateTo=data[self.__FILE_COLUMN_DATE_TO], timeTo=data[self.__FILE_COLUMN_TIME_TO],
+                            period=data[self.__FILE_COLUMN_PERIOD],
+                            dateAccuracy=data[self.__FILE_COLUMN_DATE_QUALITY],
+                            latitude=data[self.__FILE_COLUMN_LATITUDE], longitude=data[self.__FILE_COLUMN_LONGITUDE], altitude=data[self.__FILE_COLUMN_ALTITUDE], positionAccuracy=data[self.__FILE_COLUMN_POSITION_ACCURACY],
+                            massbalance_raw=data[self.__FILE_COLUMN_MASSBALANCE_RAW],
+                            density=data[self.__FILE_COLUMN_DENSITY], densityAccuracy=data[self.__FILE_COLUMN_DENSITY_ACCURACY],
+                            massbalance_we=data[self.__FILE_COLUMN_MASSBALANCE_WE], measurement_quality=data[self.__FILE_COLUMN_MASSBALANCE_QUALITY], measurement_type=data[self.__FILE_COLUMN_MASSBALANCE_TYPE],
+                            massbalance_error=data[self.__FILE_COLUMN_MASSBALANCE_ERROR], reading_error=data[self.__FILE_COLUMN_READING_ERROR], density_error=data[self.__FILE_COLUMN_DENSITY_ERROR],
+                            source=data[self.__FILE_COLUMN_SOURCE])
+
+                        self._massBalancePointCounter += 1
+
+                        self._glacier.addMassBalancePoint(massBalancePoint)
+
+
+                except Exception as e:
+
+                    errorMessage = "{0} @ {1}: {2}".format(mbp, lineCounter, e)
+                    print(errorMessage)
+                    raise
+
 
     def _getData(self, dataLine):
 
         # Dictionary with the unique values per point mass balance data line.
         data = dict()
-        # TODO: write getData method
+        p = re.compile(' +')
+        dataLineParts = p.split(dataLine)
+        while len(dataLineParts[0]) == 0:
+            dataLineParts = dataLineParts[1:]
+
+        # Filling up of the parsed data to piped into an object.
+        # ------------------------------------------------------
+        if len(dataLineParts[self.__FILE_COLUMN_NAME].strip()) == 0:
+            data[self.__FILE_COLUMN_NAME] = 'noname'
+        else:
+            data[self.__FILE_COLUMN_NAME] = dataLineParts[self.__FILE_COLUMN_NAME].strip()
+
+        # FILE_COLUMN_DATE_FROM: if date_from in vaw-files not available write date_to (mainly within _winter.dat-files)
+        if len(dataLineParts[self.__FILE_COLUMN_DATE_FROM].strip()) != 8:
+            data[self.__FILE_COLUMN_DATE_FROM] = dataLineParts[self.__FILE_COLUMN_DATE_TO].strip()
+        else:
+            data[self.__FILE_COLUMN_DATE_FROM] = dataLineParts[self.__FILE_COLUMN_DATE_FROM].strip()
+
+        data[self.__FILE_COLUMN_TIME_FROM] = self._reformatTime(dataLineParts[self.__FILE_COLUMN_TIME_FROM].strip())
+        data[self.__FILE_COLUMN_DATE_TO] = dataLineParts[self.__FILE_COLUMN_DATE_TO].strip()
+        data[self.__FILE_COLUMN_TIME_TO] = self._reformatTime(dataLineParts[self.__FILE_COLUMN_TIME_TO].strip())
+        data[self.__FILE_COLUMN_PERIOD] = float(dataLineParts[self.__FILE_COLUMN_PERIOD].strip())
+
+        # FILE_COLUMN_DATE_QUALITY: 9 was formerly used in vaw-files as unknown/undefined, in the DB undefined/unknown is 0
+        data[self.__FILE_COLUMN_DATE_QUALITY] = int(self._notNine(dataLineParts[self.__FILE_COLUMN_DATE_QUALITY].strip()))
+
+        data[self.__FILE_COLUMN_LATITUDE] = float(dataLineParts[self.__FILE_COLUMN_LATITUDE].strip())
+        data[self.__FILE_COLUMN_LONGITUDE] = float(dataLineParts[self.__FILE_COLUMN_LONGITUDE].strip())
+        data[self.__FILE_COLUMN_ALTITUDE] = float(dataLineParts[self.__FILE_COLUMN_ALTITUDE].strip())
+        # FILE_COLUMN_DATE_QUALITY: 9 was formerly used in vaw-files as unknown/undefined, in the DB undefined/unknown is 0
+        data[self.__FILE_COLUMN_POSITION_ACCURACY] = int(self._notNine(dataLineParts[self.__FILE_COLUMN_POSITION_ACCURACY].strip()))
+        data[self.__FILE_COLUMN_MASSBALANCE_RAW] = int(dataLineParts[self.__FILE_COLUMN_MASSBALANCE_RAW].strip())
+        data[self.__FILE_COLUMN_DENSITY] = int(dataLineParts[self.__FILE_COLUMN_DENSITY].strip())
+
+        # FILE_COLUMN_DATE_QUALITY: 9 was formerly used in vaw-files as unknown/undefined, in the DB undefined/unknown is 0
+        data[self.__FILE_COLUMN_DENSITY_ACCURACY] = int(self._notNine(dataLineParts[self.__FILE_COLUMN_DENSITY_ACCURACY].strip()))
+        data[self.__FILE_COLUMN_MASSBALANCE_WE] = int(dataLineParts[self.__FILE_COLUMN_MASSBALANCE_WE].strip())
+        data[self.__FILE_COLUMN_MASSBALANCE_QUALITY] = int(self._notNine(dataLineParts[self.__FILE_COLUMN_MASSBALANCE_QUALITY].strip()))
+
+        # FILE_COLUMN_MASSBALANCE_TYPE: 9 was formerly used in vaw-files as unknown/undefined, in the DB undefined/unknown is 0
+        data[self.__FILE_COLUMN_MASSBALANCE_TYPE] = int(self._notNine(dataLineParts[self.__FILE_COLUMN_MASSBALANCE_TYPE].strip()))
+        data[self.__FILE_COLUMN_MASSBALANCE_ERROR] = int(dataLineParts[self.__FILE_COLUMN_MASSBALANCE_ERROR].strip())
+        data[self.__FILE_COLUMN_READING_ERROR] = int(dataLineParts[self.__FILE_COLUMN_READING_ERROR].strip())
+        data[self.__FILE_COLUMN_DENSITY_ERROR] = int(dataLineParts[self.__FILE_COLUMN_DENSITY_ERROR].strip())
+        # TODO: FILE_COLUMN_SOURCE from abbreviations to full name conversion according to ReadMe.txt
+        data[self.__FILE_COLUMN_SOURCE] = dataLineParts[self.__FILE_COLUMN_SOURCE].strip()
 
         return (data)
+    def replace_source(self,source):
+        # TODO: function not used yet
+        source_replaced = []
+        source_dict = {"NN": "unknown",
+         "glrep": "Glaciological Reports",
+         "firep": "Firnberichte",
+         "vaw": "documents stored at VAW",
+         "vawnf": "documents stored at VAW but could not be found",
+         "kwm": "Kraftwerke Mattmark",
+         "merc/plm": "Mercanton 1916, Vermessungen am Rhonegletscher",
+         "pn": "Pro Natura",
+         "uzh": "Universität Zürich",
+         "unil": "Uni Lausanne",
+         "PSI": "Paul Scherer Institute",
+         "ab": "Andreas Bauder",
+         "mh": "Matthias Huss",
+         "mf": "Mauro Fischer",
+         "mfu/fu": "Martin Funk",
+         "al": "Andreas Linsbauer",
+         "gka": "Giovanni Kappenberger",
+         "ust": "Urs Steinegger",
+         "mt": "Michael Thalmann",
+         "pb": "Peter Beglinger",
+         "ol": "Otto Langenegger",
+         "mz": "Michael Zemp",
+         "hm": "Horst Machguth",
+         "ns": "Nadine Salzmann",
+         "jo": "Hans Oerlemanns",
+         "hz": "Harry Zekollari",
+         "ph": "Philippe Huybrechts",
+         "av": "Andreas Vieli",
+         "bm": "Boris Mueller",
+         "df": "Daniel Farinotti",
+         "jl": "Johannes Landmann"}
+        if source in source_dict:
+            source_replaced.append(source)
+        return (source_replaced)
--- dataflow/DataWriters/DatabaseWriters/MassBalancePointWriter.py
+++ dataflow/DataWriters/DatabaseWriters/MassBalancePointWriter.py
@@ -11,16 +11,16 @@ from dataflow.DataObjects.MassBalancePoint import MassBalancePoint
 
 class MassBalancePointWriter(GlamosDatabaseWriter):
     '''
-    Database writer for objects of the type point mass balance
+    Database writer for objects of the type mass balance point
 
     Attributes:
-    _MassBalancePointObservationCounter int  Counter of length-change observations written to the database
+    _MassBalancePointObservationCounter int  Counter of mass balance points written to the database
     '''
 
     _MassBalancePointObservationCounter = 0
 
     @property
-    def MassBalancePointObservationsWritten(self):
+    def massBalancePointObservationsWritten(self):
         # TODO: Description
 
         return self._MassBalancePointObservationCounter
@@ -35,5 +35,89 @@ class MassBalancePointWriter(GlamosDatabaseWriter):
 
         super().__init__(accessConfigurationFullFileName)
 
-    def isGlacierLengthChangeStored(self):
+    def isMassBalancePointStored(self):
         pass
+
+    def write(self, glacier):
+        '''
+        Writes all mass balance point observation of the given glacier into the database.
+
+        @type glacier: dataflow.DataObjects.Glacier.Glacier
+        @param glacier: Glacier object with length-change data to be written into the database
+        '''
+
+        try:
+            for massbalancePoint in glacier.massBalancePoints.values():
+                # Check if mass balance point observation is already stored in the database.
+                # The statement has to describe a SELECT which returns a unique record based on the definition of the record set.
+                # In case the mass balance data the following factors define a unique data record:
+                # - The same glacier (fk_glacier)
+                # - The same name (name)
+                # - The same start date of the observation (date_from)
+                # - The same end date of the observation (date_to)
+
+                checkStatement = "SELECT * FROM {0} WHERE fk_glacier = '{1}' AND name = '{2}' AND fk_observation_type ='{3}' AND date_from = '{4}' AND date_to = '{5}' AND latitude = '{6}' AND longitude = '{7}';".format(
+                    'mass_balance.point',
+                    glacier.pk,
+                    massbalancePoint.name,
+                    massbalancePoint.observationType,
+                    massbalancePoint.dateFrom,
+                    massbalancePoint.dateTo,
+                    massbalancePoint.latitude,
+                    massbalancePoint.longitude)
+                # Record is already in database. No further inserts needed.
+                if super().isRecordStored(checkStatement) == True:
+                    message = "The record {0} is already stored in the database. No further inserts.".format(
+                        str(massbalancePoint))
+                    print(message)
+
+                # The record is not yet stored in the database. Insert will be done.
+                else:
+                    message = "The record {0} is not yet stored in the database. Insert will be done ...".format(
+                        str(massbalancePoint))
+                    #print(message)
+
+                    # Preparing the INSERT of a not yet inserted record.
+                    insertStatement = "INSERT INTO mass_balance.point (pk, fk_glacier, name, fk_observation_type, date_from, time_from, date_to, time_to, fk_date_accuracy, period, latitude, longitude, altitude, fk_position_accuracy, massbalance_raw, density, fk_density_accuracy, massbalance_we, fk_measurement_quality, fk_measurement_type, massbalance_error, reading_error, density_error, source) VALUES ('{0}', '{1}', '{2}', {3}, '{4}', '{5}', '{6}', '{7}', {8}, {9}, {10}, {11}, {12}, {13}, {14}, {15}, {16}, {17}, {18}, {19}, {20}, {21}, {22}, '{23}');"
+
+                    # Handling possible NULL values:
+                    # TODO: if Null values possible
+                    # Handling not yet implemented values.
+                    # TODO: if values are not yet implemented
+                    # Handling not yet implemented default values.
+                    # TODO: if default values not yet implemented
+
+                    # Getting the final INSERT-statement ready.
+                    insertStatement = insertStatement.format(
+                        massbalancePoint.pk,
+                        glacier.pk,
+                        massbalancePoint.name,
+                        massbalancePoint.observationType,
+                        massbalancePoint.dateFrom, massbalancePoint.timeFrom,
+                        massbalancePoint.dateTo, massbalancePoint.timeTo,
+                        massbalancePoint.dateAccuracy,
+                        massbalancePoint.period,
+                        massbalancePoint.latitude, massbalancePoint.longitude, massbalancePoint.altitude,
+                        massbalancePoint.positionAccuracy,
+                        massbalancePoint.massbalance_raw,
+                        massbalancePoint.density, massbalancePoint.densityAccuracy,
+                        massbalancePoint.massbalance_we,
+                        massbalancePoint.measurement_quality, massbalancePoint.measurement_type,
+                        massbalancePoint.massbalance_error, massbalancePoint.reading_error, massbalancePoint.density_error,
+                        massbalancePoint.source)
+
+                    self._writeData(insertStatement)
+                    self._connection.commit()
+
+                    self._MassBalancePointObservationCounter += 1
+        except Exception as exception:
+
+            raise exception
+
+        finally:
+
+            self._connection.close()
+
+            print("\n")
+            print("-> A total of {0} mass balance point observations were inserted into the database.".format(
+                self._MassBalancePointObservationCounter))
