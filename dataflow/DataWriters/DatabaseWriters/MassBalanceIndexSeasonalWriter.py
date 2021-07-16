8538dde5fb0fc46368547a850436c0cc32cd64f1
Elias Hodel <hodel@vaw.baug.ethz.ch>
2021-07-16 11:16:07 +0200

    adapt Index Seasonal writer to GLAMOS-DB table

--- dataflow/DataWriters/DatabaseWriters/MassBalanceIndexSeasonalWriter.py
+++ dataflow/DataWriters/DatabaseWriters/MassBalanceIndexSeasonalWriter.py
@@ -53,19 +53,18 @@ class MassBalanceIndexSeasonalWriter(GlamosDatabaseWriter):
                 # In case the mass balance data the following factors define a unique data record:
                 # - The same glacier (fk_glacier)
                 # - The same name (name)
-                # - The date of daily value (date)
-                # - The values of entry (balance, accumulation, melt)
+                # - The start and end date of annual period (date_0, date_1)
+                # - The balance values of entry (b_w_meas, b_a_meas)
 
                 checkStatement = "SELECT * FROM {0} WHERE fk_glacier = '{1}' AND name = '{2}' AND " \
-                                 "date ='{3}' AND balance = '{4}' AND accumulation = '{5}' AND " \
-                                 "melt = '{6}';".format(
+                                 "date_from_annual ='{3}' AND date_to_annual = '{4}' AND b_w_meas = '{5}' AND b_a_meas = '{6}';".format(
                     'mass_balance.index_seasonal',
                     glacier.pk,
                     massbalanceIndexSeasonal.name,
-                    massbalanceIndexSeasonal.date,
-                    massbalanceIndexSeasonal.balance,
-                    massbalanceIndexSeasonal.accumulation,
-                    massbalanceIndexSeasonal.melt)
+                    massbalanceIndexSeasonal.date_0,
+                    massbalanceIndexSeasonal.date_1,
+                    massbalanceIndexSeasonal.b_w_meas,
+                    massbalanceIndexSeasonal.b_a_meas,)
 
                 # Record is already in database. No further inserts needed.
                 if super().isRecordStored(checkStatement) == True:
@@ -80,7 +79,7 @@ class MassBalanceIndexSeasonalWriter(GlamosDatabaseWriter):
                     print(message)
 
                     # Preparing the INSERT of a not yet inserted record.
-                    insertStatement = "INSERT INTO mass_balance.index_seasonal (pk, fk_glacier, name, date, balance, accumulation, melt, fk_surface_type, temperature, precipitation,reference) VALUES ('{0}', '{1}', '{2}', '{3}', '{4}', '{5}', '{6}', '{7}', {8}, {9}, '{10}');"
+                    insertStatement = "INSERT INTO mass_balance.index_seasonal (pk, fk_glacier, fk_embargo_type, fk_analysis_method_type, name, date_from_annual, date_to_annual, date_from_winter, date_to_winter, date_fall_min, date_spring_max, latitude, longitude, altitude, b_w_meas, b_a_meas, c_w_obs, c_a_obs, a_w_obs, a_a_obs, b_w_fix, b_a_fix, c_w_fix, c_a_fix, a_w_fix, a_a_fix, investigator, reference) VALUES ('{0}', '{1}', '{2}', '{3}', '{4}', '{5}', '{6}', '{7}', {8}, {9}, '{10}', '{11}', '{12}', '{13}', '{14}', '{15}', '{16}', '{17}', '{18}', '{19}', '{20}', '{21}', '{22}', '{23}', '{24}', '{25}', '{26}', '{27}', '{28}');"
 
                     # Handling possible NULL values:
                     # TODO: if Null values possible
@@ -93,14 +92,32 @@ class MassBalanceIndexSeasonalWriter(GlamosDatabaseWriter):
                     insertStatement = insertStatement.format(
                         massbalanceIndexSeasonal.pk,
                         glacier.pk,
+                        '0'. # fk_embargo_type by default 0
+                        massbalanceIndexSeasonal.analysis_method_type,
                         massbalanceIndexSeasonal.name,
-                        massbalanceIndexSeasonal.date,
-                        massbalanceIndexSeasonal.balance,
-                        massbalanceIndexSeasonal.accumulation,
-                        massbalanceIndexSeasonal.melt,
-                        massbalanceIndexSeasonal.surface_type,
-                        massbalanceIndexSeasonal.temp,
-                        massbalanceIndexSeasonal.precip_solid,
+                        massbalanceIndexSeasonal.date_0,
+                        massbalanceIndexSeasonal.date_1,
+                        massbalanceIndexSeasonal.date_fmeas,
+                        massbalanceIndexSeasonal.date_smeas,
+                        massbalanceIndexSeasonal.date_fmin,
+                        massbalanceIndexSeasonal.date_smax,
+                        massbalanceIndexSeasonal.latitude,
+                        massbalanceIndexSeasonal.longitude,
+                        massbalanceIndexSeasonal.altitude,
+                        massbalanceIndexSeasonal.b_w_meas,
+                        massbalanceIndexSeasonal.b_a_meas,
+                        massbalanceIndexSeasonal.c_w_obs,
+                        massbalanceIndexSeasonal.c_a_obs,
+                        massbalanceIndexSeasonal.a_w_obs,
+                        massbalanceIndexSeasonal.a_a_obs,
+                        massbalanceIndexSeasonal.b_w_fix,
+                        massbalanceIndexSeasonal.b_a_fix,
+                        massbalanceIndexSeasonal.c_w_fix,
+                        massbalanceIndexSeasonal.c_a_fix,
+                        massbalanceIndexSeasonal.a_w_fix,
+                        massbalanceIndexSeasonal.a_a_fix,
+
+                        massbalanceIndexSeasonal.investigator,
                         massbalanceIndexSeasonal.reference)
 
                     self._writeData(insertStatement)
